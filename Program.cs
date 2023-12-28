42087212ed61b0bf87e6c178490790e51380b424
Mustafa Zaidi <141467984+ZaidiSahab@users.noreply.github.com>
2023-12-28 23:58:09 +0500

    Add files via upload
    C#MaxAndMinFinding

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+namespace ConsoleApplication45
+{
+    class Numbers
+    {
+   
+        public static int findminimum2(int num1, int num2)
+        {
+            int min = num2;
+            if (num1 < num2) { min = num1; }
+            return min;
+        }
+
+        public static int findmax(int num1, int num2)
+        {
+            int max = num2;
+            if (num2 > num1) { max = num2; }
+            return max;
+        }
+    }
+ 
+ 
+ 
+class testnumber
+    {
+        static void Main(string[] args)
+        {
+            int min;
+            min = Numbers.findminimum2(1, 6);
+            int max =Numbers.findmax(1, 6);
+            Console.WriteLine(" Minimum Value is : " + min);
+
+            Console.WriteLine(" Maximum Value is : " + max);
+            Console.Read();
+        }
+    }
+}
